150cf7243af4f8182f33d9fcd16e0df9f813db5b
brianwong0522 <bwong03@syr.edu>
2024-03-08 21:24:44 -0500

    ListProcessor class

---
+++ ListProcessor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ListProcessor {
+    public ListProcessor(String file) {
+
+    }
+
+    public List<Process> GetList() {
+        List<Process>  list = new ArrayList<>();
+
+        return list;
+    }
+}
---
+++ SchedulerAlgorithm.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SchedulerAlgorithm {
+    Schedule  schedule;
+
+    public SchedulerAlgorithm(List<Process> list) {
+
+    }
+
+    public void DoSchedule(Schedule *schedule) {
+
+    }
+
+    public void OrderInTime() {
+        
+    }
+}
