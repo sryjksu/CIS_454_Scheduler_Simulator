54d8d6310d8746ed456cda271aa1af7ef5adcad2
brianwong0522 <bwong03@syr.edu>
2024-03-23 20:28:22 -0400

    Added ListProcessor create list code

--- ListProcessor.java
+++ ListProcessor.java
@@ -1,13 +1,40 @@
 import java.util.*;
+import java.io.*;
 
 public class ListProcessor {
-    public ListProcessor(String file) {
+    List<Process>  list = new ArrayList<>();
 
+    public ListProcessor() {
     }
 
     public List<Process> GetList() {
-        List<Process>  list = new ArrayList<>();
 
         return list;
+    }
+
+    public void createList(String file) {
+        try {
+            FileInputStream fstream = new FileInputStream(file);
+            Scanner input = new Scanner(fstream);
+            String line;
+            Scanner info;
+            Process process;
+            int pid, arrival;
+                
+            while(input.hasNext()){
+                line = input.nextLine();
+                info = new Scanner(line);
+                pid = Integer.parseInt(info.next());
+                arrival = Integer.parseInt(info.next());
+                
+                process = new Process(pid, arrival);
+                list.add(process);
+                input.close();
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }
