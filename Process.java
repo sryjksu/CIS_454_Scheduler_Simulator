778e463a09d730df69f6fa8be9554f5286bed5ce
Donghaoli0305 <89858069+Donghaoli0305@users.noreply.github.com>
2024-03-23 20:52:59 -0400

    Update Process.java

--- Process.java
+++ Process.java
@@ -5,18 +5,16 @@
 public class Process {
     private int inputTime; // Input time of the process
     private int runningTime; // Running time of the process
-    private String algorithmType; // Type of algorithm the process would like to use
+    
 
     /**
      * Constructs a new Process with specified input time, running time, and algorithm type.
      * @param inputTime The input time of the process.
      * @param runningTime The running time of the process.
-     * @param algorithmType The preferred algorithm type of the process.
      */
     public Process(int inputTime, int runningTime, String algorithmType) {
         this.inputTime = inputTime;
         this.runningTime = runningTime;
-        this.algorithmType = algorithmType;
     }
 
     // Getters and setters
@@ -53,21 +51,6 @@ public class Process {
         this.runningTime = runningTime;
     }
 
-    /**
-     * Gets the algorithm type preferred by the process.
-     * @return The algorithm type preferred by the process.
-     */
-    public String getAlgorithmType() {
-        return algorithmType;
-    }
-
-    /**
-     * Sets the algorithm type preferred by the process.
-     * @param algorithmType The algorithm type preferred by the process to set.
-     */
-    public void setAlgorithmType(String algorithmType) {
-        this.algorithmType = algorithmType;
-    }
 
     /**
      * Returns the string representation of the process.
