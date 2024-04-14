33454a5959dbac2ab5ad541ac323d2a54cceb970
kzhu540788 <119690814+kzhu540788@users.noreply.github.com>
2024-04-13 20:12:51 -0400

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -2,30 +2,18 @@ import java.util.ArrayList;
 
 public class Scheduler {
     private ArrayList<Process> processList;
-    private SchedulerAlgorithm algorithm;
+    private FIFOAlgorithm algorithm;  
 
-    public Scheduler(ArrayList<Process> processList, String algorithmType) {
+    public Scheduler(ArrayList<Process> processList) {
         this.processList = processList;
-        switch (algorithmType) {
-            case "FIFO":
-                this.algorithm = new FIFOAlgorithm();
-                break;
-            case "SJF":
-                this.algorithm = new SJFAlgorithm();
-                break;
-            case "RR":
-                this.algorithm = new RRAlgorithm();
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown algorithm type");
-        }
+        this.algorithm = new FIFOAlgorithm(); 
     }
 
     public void execute() {
-        if (this.algorithm != null) {
+        if (this.processList != null && !this.processList.isEmpty()) {
             this.algorithm.execute(this.processList);
         } else {
-            System.out.println("No scheduling algorithm set");
+            System.out.println("Process list is empty or not set");
         }
     }
 }
