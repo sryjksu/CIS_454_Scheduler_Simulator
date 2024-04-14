182b50ce75363d697eb1f0fd77461be1512fe1e2
kzhu540788 <119690814+kzhu540788@users.noreply.github.com>
2024-04-13 20:21:52 -0400

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -2,16 +2,15 @@ import java.util.ArrayList;
 
 public class Scheduler {
     private ArrayList<Process> processList;
-    private FIFOAlgorithm algorithm;  
 
     public Scheduler(ArrayList<Process> processList) {
         this.processList = processList;
-        this.algorithm = new FIFOAlgorithm(); 
     }
 
     public void execute() {
         if (this.processList != null && !this.processList.isEmpty()) {
-            this.algorithm.execute(this.processList);
+            Schedule schedule = FIFOAlgorithm.runFIFOAlgorithm(this.processList);
+            System.out.println("Schedule execution completed. Details:");
         } else {
             System.out.println("Process list is empty or not set");
         }
