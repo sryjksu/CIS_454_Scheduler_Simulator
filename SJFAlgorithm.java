510e564e9bc8a8eff8894a7ebe29333170235a88
Donghaoli0305 <89858069+Donghaoli0305@users.noreply.github.com>
2024-04-27 22:46:10 -0400

    Add files via upload

---
+++ SJFAlgorithm.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SJFAlgorithm {
+    private List<Process> inputProcesses;
+    private List<Process> readyList;
+    private List<Process> finishedList;
+    private Schedule outputSchedule;
+    private int currentTime;
+    private List<Process> readyProcesses;
+
+    public SJFAlgorithm(List<Process> inputProcesses) {
+        this.inputProcesses = inputProcesses;
+        this.readyList = new ArrayList<>();
+
+        this.finishedList = new ArrayList<>();
+        this.outputSchedule = new Schedule();
+        this.readyProcesses = new ArrayList<>();
+        this.currentTime = 0;
+    }
+    public Schedule runScheduler() {
+        return null;
+    }
+}
